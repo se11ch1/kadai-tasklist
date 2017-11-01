8af3fb4566baab56641bdec2ab8bdbab0d7375ea
seiichi <seiichi.kumagai@cloudcrossing.co.jp>
2017-11-01 08:01:12 +0000

    view user only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
   def index
-    @users = User.all.page(params[:page])
+    @user = current_user
+    # @users = User.all.page(params[:page])
   end
 
   def show
